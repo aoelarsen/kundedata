102f7b5c15c818d53e46adc951988e8ed4693f36
vinnsel <vinnsel@hotmail.com>
2024-08-22 22:21:05 +0200

    oppdatert server.js

--- server.js
+++ server.js
@@ -3,14 +3,16 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-const port = 5000;
+
+// Bruk Heroku's dynamisk tildelte port, eller 5000 som fallback lokalt
+const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-// MongoDB Atlas URI
-const uri = "mongodb+srv://sp1348:uzETy8kW83sXiHy4@cluster0.wtpcbrd.mongodb.net/rsData?retryWrites=true&w=majority&appName=Cluster0";
+// Bruk miljøvariabel for MongoDB Atlas URI
+const uri = process.env.MONGO_URI || "mongodb+srv://sp1348:uzETy8kW83sXiHy4@cluster0.wtpcbrd.mongodb.net/rsData?retryWrites=true&w=majority&appName=Cluster0";
 
 // Connect to MongoDB Atlas
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
