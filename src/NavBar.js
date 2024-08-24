3591d07645477a8058160dda9731cddbd44207c8
vinnsel <vinnsel@hotmail.com>
2024-08-24 09:35:25 +0200

    navbar desig

--- src/NavBar.js
+++ src/NavBar.js
@@ -3,23 +3,22 @@ import { Link } from 'react-router-dom';
 
 function NavBar() {
   return (
-    <nav>
-      <ul>
-        {/* Legg til linker til CustomerList og OrderList */}
-        <li>
-          <Link to="/customer-list">Customer List</Link>
+    <nav style={{ backgroundColor: '#1a202c', padding: '10px' }}>
+      <ul style={{ display: 'flex', listStyleType: 'none', margin: 0, padding: 0 }}>
+        <li style={{ marginRight: '20px' }}>
+          <Link to="/" style={{ color: '#fff', textDecoration: 'none' }}>Registrer/søk</Link>
         </li>
-        <li>
-          <Link to="/ordre">Order List</Link>
+        <li style={{ marginRight: '20px' }}>
+          <Link to="/customer-list" style={{ color: '#fff', textDecoration: 'none' }}>Kunder</Link>
         </li>
-        <li>
-          <Link to="/">Home</Link>
+        <li style={{ marginRight: '20px' }}>
+          <Link to="/ordre" style={{ color: '#fff', textDecoration: 'none' }}>Ordre</Link>
         </li>
-        <li>
-          <Link to="/service">Service</Link>
+        <li style={{ marginRight: '20px' }}>
+          <Link to="/service" style={{ color: '#fff', textDecoration: 'none' }}>Service</Link>
         </li>
-        <li>
-          <Link to="/hjelpemidler">Calculator</Link>
+        <li style={{ marginRight: '20px' }}>
+          <Link to="/hjelpemidler" style={{ color: '#fff', textDecoration: 'none' }}>Hjelpemidler</Link>
         </li>
       </ul>
     </nav>
